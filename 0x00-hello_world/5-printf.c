097985d49d36bcbcb7782e9e95b4a9d67e401158
Otumfuo Prince <princekarpture@gmail.com>
2022-12-10 11:35:01 -0800

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - prints the prase with proper grammar,
+ * but the outcome is a piece of art,
+ * Returns: 0 if exited properly, non-zero otherwise
+ */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
